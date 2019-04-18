e7556e8d77cffd66d724804120a5be22ef3f0cb2
Yu Totsuka <tty102fnio@gmail.com>
2019-04-19 00:08:11 +0900

    合成ページ素材選択ページへの遷移

--- Assets/OPS/Scripts/Presenter/MixPage/MixPagePresenter.cs
+++ Assets/OPS/Scripts/Presenter/MixPage/MixPagePresenter.cs
@@ -7,11 +7,24 @@ namespace OPS.Presenter
 
     public class MixPagePresenter : MonoBehaviour
 	{
+        [Inject]
+        MaterialSelectPagePresenter.Factory _materialSelectPageFactory;
+
+        [Inject]
+        PageManager _pageManager;
+
         UserMixModel _userMixModel;
 
         public void Setup(UserMixModel userMixModel)
         {
             _userMixModel = userMixModel;
+        }
+
+        public void OnBackClick()
+        {
+            var cpyMaterialSelectPage = _materialSelectPageFactory.Create();
+            cpyMaterialSelectPage.Setup(_userMixModel);
+            _pageManager.ChangePage(cpyMaterialSelectPage);
         }
 
         public class Factory : PlaceholderFactory<MixPagePresenter>
