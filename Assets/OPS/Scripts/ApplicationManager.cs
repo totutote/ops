8da1c0c08e1272a3ad7979fc9789d1b220b18b28
totutote <tty102fnio@gmail.com>
2019-06-04 13:47:00 +0900

    userDBが初期化されない不具合修正

--- Assets/OPS/Scripts/ApplicationManager.cs
+++ Assets/OPS/Scripts/ApplicationManager.cs
@@ -14,8 +14,11 @@ namespace OPS
 
         void Start()
         {
-            var db = new SqliteDatabase("master.sqlite3");
-            db.ForceUpdateDatabaseSchema();
+            var masterDb = new SqliteDatabase("master.sqlite3");
+            masterDb.ForceUpdateDatabaseSchema();
+
+            var userDb = new SqliteDatabase("user.sqlite3");
+            userDb.UpdateDatabaseSchema();
 
             var cpyStartPage = _startPageFactory.Create();
             _pageManager.ChangePage(cpyStartPage);
